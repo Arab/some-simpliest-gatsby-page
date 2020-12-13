0e3779ad699bb81f9ee49e910a47f7e9b37d35f0
Piotr Motak <pmotak@gmail.com>
2020-12-13 01:43:54 +0100

    add links to with-context

--- src/templates/with-context.js
+++ src/templates/with-context.js
@@ -1,8 +1,11 @@
 import React from "react"
+import { Links } from '../components/Links'
+
 const WithContext = ({ pageContext }) => (
   <section>
     <h1>{pageContext.title}</h1>
     <div dangerouslySetInnerHTML={{ __html: pageContext.content }} />
+    <Links />
   </section>
 )
 export default WithContext
